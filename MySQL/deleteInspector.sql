90828683cfbe8441ababe0e3f800fd96404ba02a
TNCEscobedo <escobedotnc@gmail.com>
2019-12-07 23:31:24 -0600

    init commit

--- MySQL/deleteInspector.sql
+++
@@ -1,9 +0,0 @@
-DROP PROCEDURE IF EXISTS deleteInspector;
-DELIMITER //
-CREATE PROCEDURE deleteInspector (IN input_idInspector INT)
-BEGIN
-DELETE FROM usuario
-WHERE usuario.idUsuario= input_idInspector;
-
-END //
-DELIMITER ;
