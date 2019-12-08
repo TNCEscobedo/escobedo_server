f61a841f3bc1ee941a99843d109376d992ef4744
TNCEscobedo <escobedotnc@gmail.com>
2019-12-08 06:59:51 -0600

    fixing bugs

--- routes/inspectores.js
+++ routes/inspectores.js
@@ -5,16 +5,13 @@ const moment = require("moment");
 const {token} = require("../middleware/token");
 const fbAuth = require("../middleware/fbAuth");
 
-router.get("/inspectores", async (req,res)=>{
+router.get("/", async (req,res)=>{
     try{
         let {fecha,fechaInicio,fechaFin} = req.query;
         if(fecha){
-            const fecha = moment(fecha).toISOString();
             const result = await db.rawQuery(`CALL getInspectoresDia(${fecha})`);
             res.send(result[0]);
         }else{
-            fechaInicio = moment(fechaInicio).toISOString();
-            fechaFin = moment(fechaFin).toISOString();
             const result = await db.rawQuery(`CALL getInspectoresIntervalo(${fechaInicio}, ${fechaFin}`);
             res.send(result[0]);
         }
